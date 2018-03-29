70a2fe688a3a20a1cedd9d2f8b993068fa34abbf
Ruslan Forostianov <ruslan@thehyve.nl>
2018-03-29 10:02:39 +0200

    Remove limits on number of found tree nodes.

--- web-app/js/datasetExplorer/ext-i2b2.js
+++ web-app/js/datasetExplorer/ext-i2b2.js
@@ -59,8 +59,6 @@ Ext.ux.OntologyTreeLoader = Ext.extend(Ext.tree.TreeLoader, {
 
     parseJson: function (response, node) {
         // shorthand
-        var Tree = Ext.tree;
-
         var concepts = Ext.decode(response.responseText);
 
         var matchList = GLOBAL.PathToExpand.split(",");
